93255f1a51ebecf6912641d97e2a6fc5b1815c4c
event_management <shamimasham7@gmail.com>
2025-10-25 00:08:47 +0600

    ready for deploy

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -66,7 +66,11 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.IsAuthenticated',  # secure by default
     # ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-      'PAGE_SIZE': 10,
+    #   'PAGE_SIZE': 10,
+    #   'COERCE_DECIMAL_TO_STRING': False,
+    #   'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
       'COERCE_DECIMAL_TO_STRING': False,
       'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -121,23 +125,23 @@ INTERNAL_IPS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {        
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME': config('dbname'),
-        'USER': config('user'),
-        'PASSWORD':config('password'),
-        'HOST':config('host'),
-        'PORT':config('port')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {        
+#         'ENGINE':'django.db.backends.postgresql',
+#         'NAME': config('dbname'),
+#         'USER': config('user'),
+#         'PASSWORD':config('password'),
+#         'HOST':config('host'),
+#         'PORT':config('port')
+#     }
+# }
 
 
 
