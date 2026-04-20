cdc404a35bc5cf0d60bcdc80ef5eef3263a34726
event_management <shamimasham7@gmail.com>
2026-04-20 21:56:36 +0600

    ready to deploy

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -87,7 +87,7 @@ TEMPLATES = [
         },
     },
 ]
-WSGI_APPLICATION = 'pet_adoption.wsgi.app'
+WSGI_APPLICATION = 'Pet_Adoption.wsgi.app'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
